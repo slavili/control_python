9b4418596ac4d2f44a66f59da2f8eb6355370c05
Vyacheslav <vilisov.v.s@yandex.ru>
2023-05-01 15:20:05 +0700

    Почти финал

--- test.py
+++ test.py
@@ -5,11 +5,19 @@ import datetime
 # Начальная установка
 if not os.path.isfile('db.csv'):
     createDb = WriteFile('db.csv', 'w')
+    del createDb
     if not os.path.isfile('AI.txt'):
         createAi = WriteFile('AI.txt', 'w')
         createAi.setLastIndex('0')
+        del createAi
 
 while True:
+    print('Команды:')
+    print('add - добавление новой заметки;')
+    print('upd - обновление добавленной ранее заметки;')
+    print('del - удаление добавленной ранее заметки;')
+    print('select - вывести все заметки на консоль')
+    print('')
     userData = input('Введите команду: ');
 
     if userData == 'add':
@@ -50,11 +58,22 @@ while True:
         print(f'Заметка обновлена')
 
     elif userData == 'del':
-        print(f'Команда на {userData}')
+        mes = ReadFile('db.csv')
+        userList = mes.getMessages()
+        id = input('Введите идентификатор удаляемой заметки: ')
+        del userList[id]
+
+        update = WriteFile('db.csv', 'w')
+        update.updateMessage(userList)
+        del update
+
+        del mes
+        print(f'Заметка удалена')
     elif userData == 'select':
         mes = ReadFile('db.csv')
         mes.getMessages(False)
         del mes
+        print()
     elif userData == 'q':
         print(f'Программа остановлена')
         break
