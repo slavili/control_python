852a4a0e10dc8687bc847c6e63c4bff01487a7ad
Vyacheslav <vilisov.v.s@yandex.ru>
2023-05-01 15:24:17 +0700

    Финишь

--- main.py
+++ main.py
@@ -5,40 +5,77 @@ import datetime
 # Начальная установка
 if not os.path.isfile('db.csv'):
     createDb = WriteFile('db.csv', 'w')
+    del createDb
     if not os.path.isfile('AI.txt'):
         createAi = WriteFile('AI.txt', 'w')
         createAi.setLastIndex('0')
+        del createAi
 
-rl = ReadFile('AI.txt')
-# print(rl.getLastIndex())
-dt_now = datetime.datetime.now()
+while True:
+    print('Команды:')
+    print('add - добавление новой заметки;')
+    print('upd - обновление добавленной ранее заметки;')
+    print('del - удаление добавленной ранее заметки;')
+    print('select - вывести все заметки на консоль')
+    print('')
+    userData = input('Введите команду: ');
 
-mes = ReadFile('db.csv')
-# mes.getMessages(False)
-# print(mes.getMessages())
-userList = mes.getMessages()
-print()
-# print(mes.getMessages())
-print(userList)
-userList['1'][0] = 'Заметка про мальчика'
-userList['1'][1] = 'Заметка про нашего мальчика дядю Фёдора!!!!'
+    if userData == 'add':
+        tempList = list()
+        rl = ReadFile('AI.txt')
+        lI = rl.getLastIndex()
+        tempList.append(f'{lI+1}')
+        title = input('Введите заголовок заметки: ');
+        tempList.append(title)
+        content = input('Введите основной текст заметки: ')
+        tempList.append(content)
+        tempList.append(f'{datetime.datetime.now()}')
+        # добавляем заметку
+        ins = WriteFile('db.csv', 'a')
+        ins.insertMessage(tempList)
+        # фиксируем AUTO_INCREMENT
+        createAi = WriteFile('AI.txt', 'w')
+        createAi.setLastIndex(f'{lI + 1}')
+        print('Заметка добавлена!')
+        del createAi
+        del ins
+        del tempList
+    elif userData == 'upd':
+        mes = ReadFile('db.csv')
+        userList = mes.getMessages()
+        id = input('Введите идентификатор редактируемой заметки: ')
+        print('Старый заголовок заметки: ', userList[id][0])
+        title = input('Введите новый заголовок заметки: ');
+        print('Старый основной текст заметки: ', userList[id][1])
+        content = input('Введите основной текст заметки: ')
+        userList[id][0] = title
+        userList[id][1] = content
+        userList[id][2] = f'{datetime.datetime.now()}'
+        del mes
+        update = WriteFile('db.csv', 'w')
+        update.updateMessage(userList)
+        del update
+        print(f'Заметка обновлена')
 
-update = WriteFile('db.csv','w')
-update.updateMessage(userList)
-update = ''
+    elif userData == 'del':
+        mes = ReadFile('db.csv')
+        userList = mes.getMessages()
+        id = input('Введите идентификатор удаляемой заметки: ')
+        del userList[id]
 
-lI = rl.getLastIndex()
-ins = WriteFile('db.csv','a')
-ins.insertMessage([f'{lI+1}','Название заметки3','Содержимое заметки3',f'{dt_now}'])
-ins = ''
+        update = WriteFile('db.csv', 'w')
+        update.updateMessage(userList)
+        del update
 
-createAi = WriteFile('AI.txt', 'w')
-createAi.setLastIndex(f'{lI+1}')
-createAi = ''
-
-# mes = ReadFile('db.csv')
-mes.getMessages(False)
-print()
-
-
-
+        del mes
+        print(f'Заметка удалена')
+    elif userData == 'select':
+        mes = ReadFile('db.csv')
+        mes.getMessages(False)
+        del mes
+        print()
+    elif userData == 'q':
+        print(f'Программа остановлена')
+        break
+    else:
+        print(f'Команда {userData} не найдена')
